7f5fdb6148aa96bb04f24fc2beb6984d29b91129
yoni31891 <39693542-yoni31891@users.noreply.replit.com>
2025-02-15 12:52:49 +0000

    User checkpoint: Set up Django project with dependencies and Replit workflow for running the server.

---
+++ replit.nix
@@ -0,0 +1,16 @@
+{pkgs}: {
+  deps = [
+    pkgs.cacert
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+  ];
+}
