3c324180f7f4eb7e8a55f4ad33a080a8a1d44980
Jonathan Shahar <yoni31891@gmail.com>
2025-02-24 15:57:21 +0200

    Add clean Render deployment configuration

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate 
